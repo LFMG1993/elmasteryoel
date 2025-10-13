8185400c941610eb251dcdf9317eca4a9d2ab7a0
LFMG1993 <lfmontes662@soy.sena.edu.co>
2025-10-12 20:00:26 -0500

    ajuste de redes minimo

--- wordpress/wp-content/themes/themeYoel/footer.php
+++ wordpress/wp-content/themes/themeYoel/footer.php
@@ -25,12 +25,6 @@
                         </svg>
                         <span>Telegram</span>
                     </a>
-                    <a href="https://www.tiktok.com/@elmasteryoel?_t=ZS-90UamYmwKJu&_r=1" target="_blank" class="flex items-center gap-x-2 text-sm hover:text-acento-dorado transition-colors">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
-                            <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
-                        </svg>
-                        <span>TikTok</span>
-                    </a>
                 </div>
             </div>
             <!-- Columna 2: Contacto -->
